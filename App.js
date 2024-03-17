0c6ab3c024bf4d075e5ac940480188508c6fa8f6
bojanavasilevska <bojanavasilevska@gmail.com>
2024-03-17 13:26:07 +0100

    Base Graphics

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Canvas, useImage, Image } from "@shopify/react-native-skia";
+import { useWindowDimensions } from "react-native";
+ 
+const App = () => {
+  const { width, height } = useWindowDimensions();
 
-export default function App() {
+  const bg = useImage(require('./assets/sprites/background-day.png'))
+  const bird = useImage(require('./assets/sprites/yellowbird-upflap.png'))
+  const pipeBottom = useImage(require('./assets/sprites/pipe-green.png'))
+  const pipeTop = useImage(require('./assets/sprites/pipe-green-top.png'))
+  const base = useImage(require('./assets/sprites/base.png'))
+
+  const pipeOffset = 0;
+
+  const r = width * 0.33;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Canvas style={{ width, height }}>
+      {/* BG */}
+      <Image image={bg} width={width} height={height} fit={'cover'}/>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      {/* PIPES */}
+      <Image image={pipeTop} y={pipeOffset - 320} x={width / 2} width={103} height={640}/>
+      <Image image={pipeBottom} y={height - 320 + pipeOffset} x={width / 2} width={103} height={640}/>
+
+      {/* BASE */}
+      <Image image={base} width={width} height={150} y={height - 75} x={0} fit={'cover'}/>
+
+      {/* BIRD */}
+      <Image image={bird} y={height / 2 } x={width / 4} width={64} height={48}/>
+    </Canvas>
+  );
+};
+ 
+export default App;
